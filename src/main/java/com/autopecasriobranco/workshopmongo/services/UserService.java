cafd2aa50d1c43c0525ef20bd50c58499c5a1bb3
Ewald Guedes Neto <ewald.neto@globo.com>
2021-05-06 00:13:33 -0300

    Insercao de usuario com POST

--- src/main/java/com/autopecasriobranco/workshopmongo/services/UserService.java
+++ src/main/java/com/autopecasriobranco/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.autopecasriobranco.workshopmongo.domain.User;
+import com.autopecasriobranco.workshopmongo.dto.UserDTO;
 import com.autopecasriobranco.workshopmongo.exception.ObjectNotFoundException;
 import com.autopecasriobranco.workshopmongo.repositories.UserRepository;
 
@@ -22,5 +23,14 @@ public class UserService {
 	
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
+	}
 }
