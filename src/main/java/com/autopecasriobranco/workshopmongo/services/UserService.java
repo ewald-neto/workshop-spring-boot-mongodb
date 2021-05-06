5336b357f46280ae7b2e07b4cc18e01cae532454
Ewald Guedes Neto <ewald.neto@globo.com>
2021-05-05 23:50:04 -0300

    Obtendo usuário por Id

--- src/main/java/com/autopecasriobranco/workshopmongo/services/UserService.java
+++ src/main/java/com/autopecasriobranco/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.autopecasriobranco.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.autopecasriobranco.workshopmongo.domain.User;
+import com.autopecasriobranco.workshopmongo.exception.ObjectNotFoundException;
 import com.autopecasriobranco.workshopmongo.repositories.UserRepository;
 
 @Service
@@ -17,4 +19,8 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	}
 }
